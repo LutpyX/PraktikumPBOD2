574c582f0b9712427178a9174919e7c098d86122
Upay <lutfifb8@gmail.com>
2026-03-18 01:10:03 +0700

    pertemuan5

---
+++ Pertemuan5(2)/Pertemuan5/src/Persegi.java
@@ -0,0 +1,62 @@
+// Nama     : Muhammad Lutfi Febriansyah
+// NIM      : 24060124140197
+// Lab      : D1
+// Tanggal  : Selasa, 17 Maret 2026
+
+public class Persegi extends BangunDatar implements IResize {
+
+    private double sisi;
+
+    public Persegi () {
+        super();
+        setJmlhSisi(4);
+    }
+
+    public Persegi (double sisi, String warna, String border) {
+        super(4, warna, border);
+        this.sisi = sisi;
+    }
+
+    public double getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(double sisi) {
+        this.sisi = sisi;
+    }
+
+    @Override
+    public double getLuas() {
+        return sisi * sisi;
+    }
+
+    @Override
+    public double getKeliling() {
+        return 4 * sisi;
+    }
+
+    public double getDiagonal() {
+        return sisi * Math.sqrt(2);
+    }
+
+    // IMPLEMENT INTERFACE
+    @Override
+    public void zoomIn() {
+        sisi = sisi * 1.1;
+    }
+
+    @Override
+    public void zoomOut() {
+        sisi = sisi * 0.9;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("Persegi:");
+        super.printInfo();
+        System.out.println("Sisi : " + sisi);
+        System.out.println("Luas : " + getLuas());
+        System.out.println("Keliling : " + getKeliling());
+        System.out.println("Diagonal : " + getDiagonal());
+    }
+}
